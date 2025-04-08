1180830d6f10dd3c2423c516130a7e608fbcb316
Luxflamy <xli2579@wisc.edu>
2025-04-08 13:21:14 -0500

    event scale 2

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -11,6 +11,8 @@ export function initEventListeners(camera, renderer) {
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
     document.addEventListener('wheel', (e) => onMouseWheel(e)); // 添加鼠标滚轮事件监听
+    document.addEventListener('gesturestart', onGestureStart); // 添加触控板缩放开始事件监听
+    document.addEventListener('gesturechange', onGestureChange); // 添加触控板缩放事件监听
 }
 
 function onWindowResize(camera, renderer) {
@@ -41,6 +43,23 @@ function onMouseWheel(event) {
     const zoomSpeed = 0.0005; // 缩放灵敏度（值越小灵敏度越低）
     targetZoom += event.deltaY * zoomSpeed; // 根据滚轮方向调整目标缩放值
     targetZoom = Math.max(1.3, Math.min(4.8, targetZoom)); // 限制缩放范围，最小值设置为1.5
+}
+
+function onGestureStart(event) {
+    event.preventDefault(); // 阻止默认行为（如页面缩放）
+}
+
+function onGestureChange(event) {
+    event.preventDefault(); // 阻止默认行为（如页面缩放）
+    const zoomSpeed = 0.07; // 缩放灵敏度
+    if (event.scale > 1) {
+        // 放大
+        targetZoom -= (event.scale - 1) * zoomSpeed;
+    } else {
+        // 缩小
+        targetZoom += (1 - event.scale) * zoomSpeed;
+    }
+    targetZoom = Math.max(1.3, Math.min(4.8, targetZoom)); // 限制缩放范围
 }
 
 export function updateZoom(camera) {
